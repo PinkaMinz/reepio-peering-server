9566314ddea22129ec860170d4072f674eebaa4f
root <root@ip-172-31-19-211.eu-central-1.compute.internal>
2017-05-23 19:34:47 +0000

    fixes some incorrect cli params

--- src/server.js
+++ src/server.js
@@ -13,22 +13,23 @@ program
 	.option('--host <n>', 'Set host', '0.0.0.0')
 	.option('--port <n>', 'Set ws port', 9001)
 	.option('--path <path>', 'Set url path', '/')
-	.option('--private-key', 'Set ssl private key', null, null)
-	.option('--public-key', 'Set ssl public key', null, null)
-	.option('--ip-limit <n>', 'Set max number of connections per ip address', null, 10)
+	.option('--private-key <path>', 'Set ssl private key', null, null)
+	.option('--public-key <path>', 'Set ssl public key', null, null)
+	.option('--ip-limit <n>', 'Set max number of connections per ip address', 10)
 	.parse(process.argv);
 
 options = {
+	key:  "reepio",
 	host: program.host,
 	port: program.port,
 	path: program.path,
-	concurrent_limit: program.ipLimit
+	concurrent_limit: parseInt(program.ipLimit)
 };
 
 if(program.privateKey && program.publicKey) {
 	options.ssl = {
 		key: fs.readFileSync( program.privateKey ),
-		certificate: fs.readFileSync( program.publicKey )
+		cert: fs.readFileSync( program.publicKey )
 	};
 }
 
